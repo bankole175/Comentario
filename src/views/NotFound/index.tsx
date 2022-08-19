97057fa1ac84631c71ec8a781b4002234e2af2fa
bhanky <samuel.bankole@quabbly.com>
2022-08-19 06:34:52 +0100

    remove unused images update not found page style

--- src/views/NotFound/index.tsx
+++ src/views/NotFound/index.tsx
@@ -2,9 +2,11 @@ import styles from './notFound.module.css'
 
 const NotFound = () => (
   <div className={styles.notFound}>
-    <h1>Page not found</h1>
-    <p>Sorry, the URL you have used may be incorrect.</p>
-    <p>Check the spelling or navigate using the menu above.</p>
+    <div>
+      <h1>Page not found</h1>
+      <p>Sorry, the URL you have used may be incorrect.</p>
+      <p>Check the spelling or navigate using the menu above.</p>
+    </div>
   </div>
 )
 
